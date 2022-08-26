5683a948c2161203542b85ed39c3e0019708b514
Bernd Porr <mail@berndporr.me.uk>
2022-08-26 10:54:55 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pure_eeg
-EEG power spectral density from two temporarily paralysed subjects
+# Pure EEG power
+
+Log Power spectra of pure EEG from two temporarily paralysed
+subjects. Data adapted from the figures in:
+
+Scalp electrical recording during paralysis: Quantitative evidence that
+EEG frequencies above 20 Hz are contaminated by EMG
+Emma M. Whitham a , Kenneth J. Pope b , Sean P. Fitzgibbon c , Trent Lewis b ,
+C. Richard Clark c , Stephen Loveless d , Marita Broberg e , Angus Wallace e ,
+Dylan DeLosAngeles e , Peter Lillie f , Andrew Hardy f , Rik
+
+by having manually clicked on the datapoints of the 6 PSD plots
+in the paper and then done a polynomial fit to smooth out the errors
+from the manual reconstruction.
+
+![alt tag](individual_average.png)
+
+## Usage
+
---
+++ plot_paralysed_EEG_PSD.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import matplotlib.pyplot as plt
+from ParalysedEEG import ParalysedEEGFromWhithamEtAl
+import numpy as np
+
+# Plot the individual PSDs
+for i in range(len(ParalysedEEGFromWhithamEtAl.allsubjectdata)):
+    p = ParalysedEEGFromWhithamEtAl(i)
+    f = np.linspace(p.f_signal_min,p.f_signal_max,100)
+    psd = p.EEGVariance(f)
+    totalEEGPower = p.totalEEGPower()
+    print("Average EEG voltage is (uV):",round((totalEEGPower**0.5)*1E6))
+    plt.plot(f,psd,linewidth=1,label="dataset #{}".format(i))
+
+# Plot the average (no arguments needed here for the constructor)
+p = ParalysedEEGFromWhithamEtAl(i)
+f = np.linspace(p.f_signal_min,p.f_signal_max,100)
+psd = p.EEGVariance(f)
+totalEEGPower = p.totalEEGPower()
+print("Average EEG voltage is (uV):",round((totalEEGPower**0.5)*1E6))
+
+plt.ylim([-16,-10])
+plt.xlim([0,100])
+plt.xlabel("f/Hz")
+plt.ylabel("Log Power (V^2 / Hz)")
+plt.plot(f,psd,linewidth=3,color="0",label="Average")
+plt.legend()
+plt.show()
